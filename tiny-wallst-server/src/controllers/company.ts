a4409b9b153d934c93233acb0bbd1cabe7aaf888
Vincent Tsai <vincentvtt@hotmail.com>
2021-11-04 01:26:03 +1100

    fix: add dist to gitignore

--- tiny-wallst-server/src/controllers/company.ts
+++ tiny-wallst-server/src/controllers/company.ts
@@ -1,9 +1,9 @@
 import { Context } from "koa";
-import { getManager } from "typeorm";
+import { getRepository } from "typeorm";
 import { SwsCompany } from "./../entities/SwsCompany";
 
-export async function getAllCompanies(context: Context) {
-  const companyRepository = getManager().getRepository(SwsCompany);
+export async function getAllCompanies(context: Context): Promise<SwsCompany[]> {
+  const companyRepository = getRepository(SwsCompany);
   const allCompanies = await companyRepository.find({});
-  context.body = allCompanies;
+  return (context.body = allCompanies);
 }
